2ddd2a8f2c916f6f2bdf8e4af8062de77868e35d
Blen Wondimagegn <mekuriablen@gmail.com>
2020-08-28 01:43:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,23 +1,23 @@
-class Song 
-attr_accessor :name, :artist, :genre
-@@count = 0
- @@artist=Array.new 
-@@genre=Array.new 
+# class Song 
+# attr_accessor :name, :artist, :genre
+# @@count = 0
+# @@artist=Array.new 
+# @@genre=Array.new 
 
-def initialize(name,artist,genre)
-  # @@count += 1
-  #   @name = name 
-  #   @artist= artist
-  #   @genre = genre
-  #   @@artist.push(artist)
-  #   @@genre << genre
+# def initialize(name,artist,genre)
+#   # @@count += 1
+#   #   @name = name 
+#   #   @artist= artist
+#   #   @genre = genre
+#   #   @@artist.push(artist)
+#   #   @@genre << genre
   
-  @name=name
-    @artist=artist
-    @genre=genre
-    @@count+=1
-    @@artists.push(artist)
-    @@genres.push(genre)
-end 
+#   @name=name
+#     @artist=artist
+#     @genre=genre
+#     @@count+=1
+#     @@artists.push(artist)
+#     @@genres.push(genre)
+# end 
 
-end
+# end
