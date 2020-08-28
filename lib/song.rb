d7e042439fb54db59b48c0e2a1b7e631b0144894
Blen Wondimagegn <mekuriablen@gmail.com>
2020-08-28 01:39:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,8 @@
 class Song 
 attr_accessor :name, :artist, :genre
 @@count = 0
-@@genres = []
-@@artist = []
+@@genres = Array.new 
+@@artist = Array.new 
 
 def initialize(name,artist,genre)
    @@count += 1
