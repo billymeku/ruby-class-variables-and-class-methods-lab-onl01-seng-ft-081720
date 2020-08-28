21eb597959dc6b9e8af6366c7e5fc02c1414aab4
Blen Wondimagegn <mekuriablen@gmail.com>
2020-08-28 02:03:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,6 +31,28 @@ def initialize(name, artist, genre)
     g ={}
     @@genres.each do |geners,songs|
       @@geners
+      def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if !genre_count[genre]
+        genre_count[genre] = 0
+      end
+      genre_count[genre] += 1
+    end
+    genre_count
+  end
+
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if !artist_count[artist]
+        artist_count[artist] = 0
+      end
+      artist_count[artist] += 1
+    end
+    artist_count
+  end
+end
       
     end 
   end 
